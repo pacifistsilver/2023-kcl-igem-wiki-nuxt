b610261408379dbfe4810140ce749d705df0c0af
daniel luo <danielluo1143@gmail.com>
2023-07-29 00:09:47 +0100

    feat: team member highlighting

--- pages/members.vue
+++ pages/members.vue
@@ -18,8 +18,8 @@
                     <img data-src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822" class="person-img" alt="DOG" src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Asmae Ait Abdallah</p>
-                    <p class="team-tabs-person-name-desc2">Team Leader</p>
+                    <p class="team-tabs-person-name-title">Asmae Ait Abdallah</p>
+                    <p class="team-tabs-person-name-desc">Team Leader</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -27,8 +27,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/julia.jpg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/julia.jpg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Julija Gellere</p>
-                    <p class="team-tabs-person-name-desc2">Team Leader</p>
+                    <p class="team-tabs-person-name-title">Julija Gellere</p>
+                    <p class="team-tabs-person-name-desc">Team Leader</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -36,8 +36,8 @@
                     <img data-src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822" class="person-img" alt="DOG" src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">I Cheng Kan</p>
-                    <p class="team-tabs-person-name-desc2">Dry Lab</p>
+                    <p class="team-tabs-person-name-title">I Cheng Kan</p>
+                    <p class="team-tabs-person-name-desc">Dry Lab</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -45,8 +45,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/tiffie.jpeg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/tiffie.jpeg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Tiffanie Dihardjo</p>
-                    <p class="team-tabs-person-name-desc2">Wet Lab</p>
+                    <p class="team-tabs-person-name-title">Tiffanie Dihardjo</p>
+                    <p class="team-tabs-person-name-desc">Wet Lab</p>
                   </div>
                 </div>
               </div>
@@ -59,8 +59,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/pema.png" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/pema.png">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Pema Tsering</p>
-                    <p class="team-tabs-person-name-desc2">Dry Lab</p>
+                    <p class="team-tabs-person-name-title">Pema Tsering</p>
+                    <p class="team-tabs-person-name-desc">Dry Lab</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -68,8 +68,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/hamza.jpg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/hamza.jpg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Hamza Haji Nur</p>
-                    <p class="team-tabs-person-name-desc2">Social Media</p>
+                    <p class="team-tabs-person-name-title">Hamza Haji Nur</p>
+                    <p class="team-tabs-person-name-desc">Social Media</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -77,8 +77,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/dan.jpg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/dan.jpg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Daniel Wei Luo</p>
-                    <p class="team-tabs-person-name-desc2">Dry Lab</p>
+                    <p class="team-tabs-person-name-title">Daniel Wei Luo</p>
+                    <p class="team-tabs-person-name-desc">Dry Lab</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -86,8 +86,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/gigi.jpg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/gigi.jpg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Wing Gee Shum</p>
-                    <p class="team-tabs-person-name-desc2">Social Media</p>
+                    <p class="team-tabs-person-name-title">Wing Gee Shum</p>
+                    <p class="team-tabs-person-name-desc">Social Media</p>
                   </div>
                 </div>
               </div>
@@ -99,8 +99,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/nicole.jpeg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/nicole.jpeg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Nicole Salem </p>
-                    <p class="team-tabs-person-name-desc2">Human Practices</p>
+                    <p class="team-tabs-person-name-title">Nicole Salem </p>
+                    <p class="team-tabs-person-name-desc">Human Practices</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -108,8 +108,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/rheima.jpeg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/rheima.jpeg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Rheima Saptari</p>
-                    <p class="team-tabs-person-name-desc2">Wetlab</p>
+                    <p class="team-tabs-person-name-title">Rheima Saptari</p>
+                    <p class="team-tabs-person-name-desc">Wetlab</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -117,8 +117,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/jon.jpeg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/jon.jpeg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Jonathan Collard </p>
-                    <p class="team-tabs-person-name-desc2">Human Practices</p>
+                    <p class="team-tabs-person-name-title">Jonathan Collard </p>
+                    <p class="team-tabs-person-name-desc">Human Practices</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -126,8 +126,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/sri.jpeg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/sri.jpeg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Sri Gullapalli</p>
-                    <p class="team-tabs-person-name-desc2">Wetlab</p>
+                    <p class="team-tabs-person-name-title">Sri Gullapalli</p>
+                    <p class="team-tabs-person-name-desc">Wetlab</p>
                   </div>
                 </div>
               </div>
@@ -140,8 +140,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/kati.jpg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/kati.jpg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Katalin Kovacs</p>
-                    <p class="team-tabs-person-name-desc2">Wetlab</p>
+                    <p class="team-tabs-person-name-title">Katalin Kovacs</p>
+                    <p class="team-tabs-person-name-desc">Wetlab</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -149,8 +149,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/dan2.jpg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/dan2.jpg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Daniella Cetta</p>
-                    <p class="team-tabs-person-name-desc2">Wetlab</p>
+                    <p class="team-tabs-person-name-title">Daniella Cetta</p>
+                    <p class="team-tabs-person-name-desc">Wetlab</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -158,8 +158,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/wren.jpg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/wren.jpg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Uyen Tran</p>
-                    <p class="team-tabs-person-name-desc2">Wetlab</p>
+                    <p class="team-tabs-person-name-title">Uyen Tran</p>
+                    <p class="team-tabs-person-name-desc">Wetlab</p>
                   </div>
                 </div>
                 <div class = "teams-tab-person col-lg-3">
@@ -167,8 +167,8 @@
                     <img data-src="https://static.igem.wiki/teams/4584/wiki/member-pics/margherita.jpeg" class="person-img" alt="DOG" src="https://static.igem.wiki/teams/4584/wiki/member-pics/margherita.jpeg">
                   </div>
                   <div class = "teams-tab-person-info">
-                    <p class="team-tabs-person-name-desc1">Margherita Autelitano</p>
-                    <p class="team-tabs-person-name-desc2">Drylab</p>
+                    <p class="team-tabs-person-name-title">Margherita Autelitano</p>
+                    <p class="team-tabs-person-name-desc">Drylab</p>
                   </div>
                 </div>
               </div>
