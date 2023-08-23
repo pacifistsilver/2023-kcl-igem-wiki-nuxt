cff3891d11284cd6db0ff1abaf15976840883ae5
Daniel <danielluo1143@gmail.com>
2023-08-23 17:58:01 +0100

    feat: home page

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,14 @@
         </div>
     </div>
   </section>
+  <section class = "index-stats">
+    <div class = "container">
+        <div class="col lg-6 w-50 mx-auto" >
+          <h2 class = "header-title text-center" data-aos="fade-up" data-aos-duration="1000">Introducing ColoEcho!</h2>
+          <p class = "header-text" data-aos="fade-up" data-aos-duration="1200"> Welcome to KCL's 2023 iGEM Wiki! We're still working on our project, but make sure to check out our project description. Check back later in the summer for more updates!</p>
+        </div>
+    </div>
+  </section>
 </template>
 <script>
 </script>
