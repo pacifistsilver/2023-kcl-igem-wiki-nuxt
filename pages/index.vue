ebf95821fd428ce42b3a46a8e1d06a36fffa1528
Daniel <danielluo1143@gmail.com>
2023-08-23 18:22:17 +0100

    fix: forgor to add index

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,29 @@
 <script lang="ts" setup></script>
 <!-- ideas: risk quiz -->
 <template>
-  <section class = "index-header">
-    <div class = "container">
-        <div class="col lg-6 w-50 mx-auto" >
-          <h2 class = "header-title text-center" data-aos="fade-up" data-aos-duration="1000">Introducing ColoEcho!</h2>
-          <p class = "header-text" data-aos="fade-up" data-aos-duration="1200"> Welcome to KCL's 2023 iGEM Wiki! We're still working on our project, but make sure to check out our project description. Check back later in the summer for more updates!</p>
+  <section class ="index-wrapper"> 
+    <section class = "index-header">
+      <div class = "container">
+          <div class="w-50 mx-auto" >
+            <h2 class = "index-title text-center" data-aos="fade-up" data-aos-duration="1000">Introducing ColoEcho!</h2>
+            <p class = "header-text" data-aos="fade-up" data-aos-duration="1200"> Welcome to KCL's 2023 iGEM Wiki! We're still working on our project, but make sure to check out our project description. Check back later in the summer for more updates!</p>
+          </div>
+      </div>
+    </section>
+    <section class = "index-stats">
+      <div class = "container">
+        <div class = "row">
+            <div class="col lg-2" >
+              <h2 class = "index-title" data-aos="fade-up" data-aos-duration="1000">Global Statistics</h2>
+              <p class = "header-text" data-aos="fade-up" data-aos-duration="1200"> In your face statistics in the larger, bolder font. Use this section for the side text. </p>
+            </div>
+            <div class="col lg-10" data-aos="fade-up" data-aos-duration="900">
+              <img data-src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822" class="person-img" alt="DOG" src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822">
+            </div>
         </div>
-    </div>
-  </section>
-  <section class = "index-stats">
-    <div class = "container">
-        <div class="col lg-6 w-50 mx-auto" >
-          <h2 class = "header-title text-center" data-aos="fade-up" data-aos-duration="1000">Introducing ColoEcho!</h2>
-          <p class = "header-text" data-aos="fade-up" data-aos-duration="1200"> Welcome to KCL's 2023 iGEM Wiki! We're still working on our project, but make sure to check out our project description. Check back later in the summer for more updates!</p>
-        </div>
-    </div>
-  </section>
+      </div>
+    </section>
+  </section>  
 </template>
 <script>
 </script>
