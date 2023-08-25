ae160dad41811d10f37abbae9d4b0700a881a68b
daniel luo <danielluo1143@gmail.com>
2023-08-25 11:11:52 +0100

    feat: home page changes

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,26 @@
   <section class ="index-wrapper"> 
     <section class = "index-header">
       <div class = "container">
-          <div class="w-50 mx-auto" >
-            <h2 class = "index-title text-center" data-aos="fade-up" data-aos-duration="1000">Introducing ColoEcho!</h2>
-            <p class = "header-text" data-aos="fade-up" data-aos-duration="1200"> Welcome to KCL's 2023 iGEM Wiki! We're still working on our project, but make sure to check out our project description. Check back later in the summer for more updates!</p>
+          <div class="title" >
+            <h1 class = "header-text"> 
+              <span data-aos ="fade-up" data-aos-duration="500" data-aos-easing = "ease-in-out"> 
+                <span class = "white">We are</span>
+                developing a new diagnostic</span>
+              <span data-aos ="fade-up" data-aos-duration="2000" data-aos-easing = "ease-in-out">
+                <span class = "white"> tool to create a better</span> 
+                Colon Cancer diagnosis.
+              </span>
+            </h1>
           </div>
       </div>
+
     </section>
     <section class = "index-stats">
       <div class = "container">
         <div class = "row">
             <div class="col lg-2" >
               <h2 class = "index-title" data-aos="fade-up" data-aos-duration="1000">Global Statistics</h2>
-              <p class = "header-text" data-aos="fade-up" data-aos-duration="1200"> In your face statistics in the larger, bolder font. Use this section for the side text. </p>
+              <p class = "index-text" data-aos="fade-up" data-aos-duration="1200"> In your face statistics in the larger, bolder font. Use this section for the side text. </p>
             </div>
             <div class="col lg-10" data-aos="fade-up" data-aos-duration="900">
               <img data-src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822" class="person-img" alt="DOG" src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822">
