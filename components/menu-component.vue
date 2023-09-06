99943b20c9885af1f2cbb21a4dfea4688d27ec07
daniel luo <danielluo1143@gmail.com>
2023-09-06 01:19:26 +0100

    feat: hamburger menu implemented

--- components/menu-component.vue
+++ components/menu-component.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-xl fixed-top" id="navbar">
+    <nav class="navbar navbar-expand-xl" id="navbar">
         <div class="container" id="navbar-container">
             <!---- TEAM NAME ---->
             <a class = "navbar-brand" href="/kcl-uk/">
@@ -7,25 +7,16 @@
             </a>
 
             <!---- SMALL SCREEN MENU ICON ---->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+
+
+            <button class="hamburger hamburger--spin" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent" >
                 <ul class="navbar-nav ml-auto left-aligned">
-
-                    <!---- TEAM ---->
-                    <li class="nav-item dropdown">
-                        <NuxtLink class = "nav-link dropdown-toggle" id="navbar-item" role="button" data-bs-toggle="dropdown" aria-expanded="false" to="./team">
-                            Team
-                        </NuxtLink>
-                        <ul class="dropdown-menu" aria-labelledby="navbar item">
-                            <li><NuxtLink class="dropdown-item" to="./members">Members</NuxtLink></li>
-                            <li><NuxtLink class="dropdown-item" to="./attributions">Attributions</NuxtLink></li>
-                        </ul>
-                    </li>
-
                     <!---- PROJECT ---->
                     <li class="nav-item dropdown">
                         <NuxtLink class = "nav-link dropdown-toggle" id="navbar-item" role="button" data-bs-toggle="dropdown" aria-expanded="false" to="">
@@ -65,6 +56,16 @@
                         <NuxtLink class="nav-link" id="navbar-item"  to="./results">Results</NuxtLink>
                     </li>
 
+                    <li class="nav-item dropdown">
+                        <NuxtLink class = "nav-link dropdown-toggle" id="navbar-item" role="button" data-bs-toggle="dropdown" aria-expanded="false" to="./team">
+                            Team
+                        </NuxtLink>
+                        <ul class="dropdown-menu" aria-labelledby="navbar item">
+                            <li><NuxtLink class="dropdown-item" to="./members">Members</NuxtLink></li>
+                            <li><NuxtLink class="dropdown-item" to="./attributions">Attributions</NuxtLink></li>
+                        </ul>
+                    </li>
+
                 </ul>
             </div>
         </div>
@@ -73,22 +74,28 @@
 <script>
 export default {
   mounted() {
-      this.$nextTick(function(){
+    this.$nextTick(function(){
         window.addEventListener("scroll", function(){
-          var navbar = document.getElementById("navbar")
-          var nav_classes = navbar.classList
-          if(document.documentElement.scrollTop >= 150) {
-            if (nav_classes.contains("navbar-scrolled") === false) {
-              nav_classes.toggle("navbar-scrolled");
+            var navbar = document.getElementById("navbar")
+            var nav_classes = navbar.classList
+            if(document.documentElement.scrollTop >= 150) {
+                if (nav_classes.contains("navbar-scrolled") === false) {
+                    nav_classes.toggle("navbar-scrolled");
             }
-          }
-          else {
-            if (nav_classes.contains("navbar-scrolled") === true) {
-              nav_classes.toggle("navbar-scrolled");
             }
-          }
+            else {
+                if (nav_classes.contains("navbar-scrolled") === true) {
+                    nav_classes.toggle("navbar-scrolled");
+            }
+            }
         })
-      })
-    },
-}
+        window.addEventListener('click', function() {
+            // Toggle class "is-active"
+            var hamburger = document.querySelector(".hamburger");
+            hamburger.classList.toggle("is-active");
+            
+            // Do something else, like open/close menu
+    })
+})
+}}
 </script>
