99131fac8b972d7a5e7395829b1f655ffcfe965c
daniel luo <danielluo1143@gmail.com>
2023-09-29 01:51:39 +0100

    menu new css rules

--- components/menu-component.vue
+++ components/menu-component.vue
@@ -1,188 +1,165 @@
 <template>
-    <nav class="navbar navbar-expand-xl" id="navbar">
-        <div class="container" id="navbar-container">
-            <!---- TEAM NAME ---->
-            <a class="navbar-brand" href="/kcl-uk/">
-                <img id="navbar-brand img"
-                    src="https://static.igem.wiki/teams/4584/wiki/blue-logo-name-white-background-removebg-preview.png">
-            </a>
+    <div class="menuButton">
+        <button class="hamburger hamburger--spin" type="button" aria-label="Toggle navigation">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+        </button>
+    </div>
 
-            <!---- SMALL SCREEN MENU ICON ---->
-
-            <div class="menuButton">
-                <button class="hamburger hamburger--spin" type="button"
-                    aria-label="Toggle navigation">
-                    <span class="hamburger-box">
-                        <span class="hamburger-inner"></span>
-                    </span>
-                </button>
+    <div class="p-menu" id="menu" data-is-menu-opened="false" tabindex="-1">
+        <div class="p-menu__inner" style="height: 526px;">
+            <div class="p-menu__contents">
+                <nav class="p-menuNav" aria-label="ナビゲーションメニュー" itemscope="" itemprop="hasPart"
+                    itemtype="http://schema.org/SiteNavigationElement">
+                    <ul class="p-menuNavList">
+                        <li class="p-menuNavListItem" itemprop="hasPart" itemscope="" itemtype="http://schema.org/WebPage">
+                            <a class="p-menuNavListItem__inner" itemprop="url" href="/" data-path-id="index"
+                                aria-current="false"><span class="p-menuNavListItem__label" itemprop="name">Home</span></a>
+                        </li>
+                        <li class="p-menuNavListItem" itemprop="hasPart" itemscope="" itemtype="http://schema.org/WebPage">
+                            <a class="p-menuNavListItem__inner " itemprop="url" href="/project/" data-path-id="wedo"
+                                aria-current="false"><span class="p-menuNavListItem__label"
+                                    itemprop="name">Project</span></a>
+                        </li>
+                        <li class="p-menuNavListItem" itemprop="hasPart" itemscope="" itemtype="http://schema.org/WebPage">
+                            <a class="p-menuNavListItem__inner " itemprop="url" href="/safety/" data-path-id="result"
+                                aria-current="false"><span class="p-menuNavListItem__label"
+                                    itemprop="name">Safety</span></a>
+                        </li>
+                        <li class="p-menuNavListItem" itemprop="hasPart" itemscope="" itemtype="http://schema.org/WebPage">
+                            <a class="p-menuNavListItem__inner " itemprop="url" href="/humanprac/" data-path-id="career"
+                                aria-current="page"><span class="p-menuNavListItem__label" itemprop="name">Human
+                                    Practices</span></a>
+                        </li>
+                        <li class="p-menuNavListItem" itemprop="hasPart" itemscope="" itemtype="http://schema.org/WebPage">
+                            <a class="p-menuNavListItem__inner " itemprop="url" href="/awards/" data-path-id="news"
+                                aria-current="false"><span class="p-menuNavListItem__label"
+                                    itemprop="name">Awards</span></a>
+                        </li>
+                        <li class="p-menuNavListItem" itemprop="hasPart" itemscope="" itemtype="http://schema.org/WebPage">
+                            <a class="p-menuNavListItem__inner " itemprop="url" href="/results/" target="_blank"
+                                data-path-id="blog" aria-current="false"><span class="p-menuNavListItem__label"
+                                    itemprop="name">Results</span></a>
+                        </li>
+                        <li class="p-menuNavListItem" itemprop="hasPart" itemscope="" itemtype="http://schema.org/WebPage">
+                            <a class="p-menuNavListItem__inner " itemprop="url" href="/team/" data-path-id="company"
+                                aria-current="false"><span class="p-menuNavListItem__label" itemprop="name">Team</span></a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
+        </div>
+    </div>
+    <nav class="navbar p-header" id="navbar">
+        <a class="p-headerLogo" href="/kcl-uk/">
+            <img src="https://static.igem.wiki/teams/4584/wiki/blue-logo-name-white-background-removebg-preview.png">
+        </a>
 
-            <div class="p-menu" id="menu" data-is-menu-opened="false" tabindex="-1">
-                <div class="p-menu__inner" style="height: 526px;">
-                    <div class="p-menu__contents">
-                        <nav class="p-menuNav" aria-label="ナビゲーションメニュー" itemscope="" itemprop="hasPart"
-                            itemtype="http://schema.org/SiteNavigationElement">
-                            <ul class="p-menuNavList">
-                                <li class="p-menuNavListItem" itemprop="hasPart" itemscope=""
-                                    itemtype="http://schema.org/WebPage">
-                                    <a class="p-menuNavListItem__inner" itemprop="url" href="/"
-                                        data-path-id="index" aria-current="false"><span class="p-menuNavListItem__label"
-                                            itemprop="name">Home</span></a>
-                                </li>
-                                <li class="p-menuNavListItem" itemprop="hasPart" itemscope=""
-                                    itemtype="http://schema.org/WebPage">
-                                    <a class="p-menuNavListItem__inner " itemprop="url" href="/wedo/"
-                                        data-path-id="wedo" aria-current="false"><span class="p-menuNavListItem__label"
-                                            itemprop="name">We Do</span></a>
-                                </li>
-                                <li class="p-menuNavListItem" itemprop="hasPart" itemscope=""
-                                    itemtype="http://schema.org/WebPage">
-                                    <a class="p-menuNavListItem__inner " itemprop="url" href="/result/"
-                                        data-path-id="result" aria-current="false"><span class="p-menuNavListItem__label"
-                                            itemprop="name">Result</span></a>
-                                </li>
-                                <li class="p-menuNavListItem" itemprop="hasPart" itemscope=""
-                                    itemtype="http://schema.org/WebPage">
-                                    <a class="p-menuNavListItem__inner " itemprop="url" href="/career/"
-                                        data-path-id="career" aria-current="page"><span class="p-menuNavListItem__label"
-                                            itemprop="name">Career</span></a>
-                                </li>
-                                <li class="p-menuNavListItem" itemprop="hasPart" itemscope=""
-                                    itemtype="http://schema.org/WebPage">
-                                    <a class="p-menuNavListItem__inner " itemprop="url" href="/news/"
-                                        data-path-id="news" aria-current="false"><span class="p-menuNavListItem__label"
-                                            itemprop="name">News</span></a>
-                                </li>
-                                <li class="p-menuNavListItem" itemprop="hasPart" itemscope=""
-                                    itemtype="http://schema.org/WebPage">
-                                    <a class="p-menuNavListItem__inner " itemprop="url" href="/blog/"
-                                        target="_blank" data-path-id="blog" aria-current="false"><span
-                                            class="p-menuNavListItem__label" itemprop="name">Blog</span></a>
-                                </li>
-                                <li class="p-menuNavListItem" itemprop="hasPart" itemscope=""
-                                    itemtype="http://schema.org/WebPage">
-                                    <a class="p-menuNavListItem__inner " itemprop="url" href="/company/"
-                                        data-path-id="company" aria-current="false"><span class="p-menuNavListItem__label"
-                                            itemprop="name">Company</span></a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-                <div class="c-roundedButton p-menuCloseButton">
-                    <button class="c-roundedButton__inner p-menuCloseButton__inner js-menuCloseButton" aria-label="メニューを閉じる"
-                        type="button">
-                        <span class="c-roundedButton__label p-menuCloseButton__label">Close</span>
-                    </button>
-                </div>
-            </div>
+        <div class="p-header__inner">
+            <ul class="navbar-nav p-headerNav">
+                <!---- PROJECT ---->
+                <li class="nav-item dropdown p-headerNavListItem">
+                    <NuxtLink class="nav-link dropdown-toggle" id="navbar-item" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false" to="">
+                        <span class="menuNavItem__label">Project</span>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto left-aligned">
-                    <!---- PROJECT ---->
-                    <li class="nav-item dropdown">
-                        <NuxtLink class="nav-link dropdown-toggle" id="navbar-item" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false" to="">
-                            <span class="menuNavItem__label">Project</span>
+                    </NuxtLink>
+                    <ul class="dropdown-menu" aria-labelledby="navbar-item">
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./description">
+                                <span>Description</span>
+                            </NuxtLink>
+                        </li>
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./model">
+                                <span>Model</span>
 
-                        </NuxtLink>
-                        <ul class="dropdown-menu" aria-labelledby="navbar-item">
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./description">
-                                    <span>Description</span>
-                                </NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./model">
-                                    <span>Model</span>
-
-                                </NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./experiments">
-                                    <span>Experiments</span>
+                            </NuxtLink>
+                        </li>
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./experiments">
+                                <span>Experiments</span>
 
 
-                                </NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./engineering">
-                                    <span>Engineering</span>
+                            </NuxtLink>
+                        </li>
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./engineering">
+                                <span>Engineering</span>
 
 
-                                </NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./results">
-                                    <span>Results</span>
+                            </NuxtLink>
+                        </li>
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./results">
+                                <span>Results</span>
 
 
-                                </NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./notebook">
-                                    <span>Notebook</span>
-                                </NuxtLink>
-                            </li>
-                        </ul>
-                    </li>
+                            </NuxtLink>
+                        </li>
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./notebook">
+                                <span>Notebook</span>
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </li>
 
-                    <!---- SAFETY ---->
-                    <li class="nav-item">
-                        <NuxtLink class="nav-link" id="navbar-item" to="./safety">
-                            <span class="menuNavItem__label">Safety</span>
-                        </NuxtLink>
-                    </li>
+                <!---- SAFETY ---->
+                <li class="nav-item p-headerNavListItem">
+                    <NuxtLink class="nav-link" id="navbar-item" to="./safety">
+                        <span class="menuNavItem__label">Safety</span>
+                    </NuxtLink>
+                </li>
 
-                    <!---- HUMAN PRACTICES ---->
-                    <li class="nav-item">
-                        <NuxtLink class="nav-link" id="navbar-item" to="./human-practices">
-                            <span class="menuNavItem__label">Human Practices</span>
-                        </NuxtLink>
-                    </li>
+                <!---- HUMAN PRACTICES ---->
+                <li class="nav-item p-headerNavListItem">
+                    <NuxtLink class="nav-link" id="navbar-item" to="./human-practices">
+                        <span class="menuNavItem__label">Human Practices</span>
+                    </NuxtLink>
+                </li>
 
-                    <!---- AWARDS ---->
-                    <li class="nav-item dropdown">
-                        <NuxtLink class="nav-link dropdown-toggle" id="navbar-item" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false" to="./">
-                            <span class="menuNavItem__label">Awards</span>
-                        </NuxtLink>
-                        <ul class="dropdown-menu" aria-labelledby="navbar-item">
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./parts">New Basic Part</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./ihp">Integrated Human Practices</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./education">Education</NuxtLink>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <NuxtLink class="nav-link" id="navbar-item" to="./results">
-                            <span class="menuNavItem__label">Results</span>
-                        </NuxtLink>
-                    </li>
+                <!---- AWARDS ---->
+                <li class="nav-item dropdown p-headerNavListItem">
+                    <NuxtLink class="nav-link dropdown-toggle" id="navbar-item" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false" to="./">
+                        <span class="menuNavItem__label">Awards</span>
+                    </NuxtLink>
+                    <ul class="dropdown-menu" aria-labelledby="navbar-item">
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./parts">New Basic Part</NuxtLink>
+                        </li>
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./ihp">Integrated Human Practices</NuxtLink>
+                        </li>
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./education">Education</NuxtLink>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item p-headerNavListItem">
+                    <NuxtLink class="nav-link" id="navbar-item" to="./results">
+                        <span class="menuNavItem__label">Results</span>
+                    </NuxtLink>
+                </li>
 
-                    <li class="nav-item dropdown">
-                        <NuxtLink class="nav-link dropdown-toggle" id="navbar-item" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false" to="./team">
-                            <span class="menuNavItem__label">Team</span>
-                        </NuxtLink>
-                        <ul class="dropdown-menu" aria-labelledby="navbar item">
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./members">Members</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink class="dropdown-item" to="./attributions">Attributions</NuxtLink>
-                            </li>
-                        </ul>
-                    </li>
+                <li class="nav-item dropdown p-headerNavListItem">
+                    <NuxtLink class="nav-link dropdown-toggle" id="navbar-item" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false" to="./team">
+                        <span class="menuNavItem__label">Team</span>
+                    </NuxtLink>
+                    <ul class="dropdown-menu" aria-labelledby="navbar item">
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./members">Members</NuxtLink>
+                        </li>
+                        <li class = "p-headerNavListItem">
+                            <NuxtLink class="dropdown-item" to="./attributions">Attributions</NuxtLink>
+                        </li>
+                    </ul>
+                </li>
 
-                </ul>
-            </div>
+            </ul>
         </div>
     </nav>
 </template>  
