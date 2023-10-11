864bee83ecf6c9ff194f544270b7e93284cadf9c
Asmae <k21007054@kcl.ac.uk>
2023-10-11 18:58:48 +0100

    added globe image

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,65 @@
 <script lang="ts" setup></script>
 <!-- ideas: risk quiz -->
 <template>
-    <div class="bgObjects" aria-hidden="true">
-      <div class="bgObjectItem">
-        <div class="bgObjectItem-Inner ">
-          <div class = "js-image" style="animation-duration: 1.13207s; animation-delay: -3.16601s;" alt="" decoding="async"></div>
+  <div class="bgObjects" aria-hidden="true">
+    <div class="bgObjectItem">
+      <div class="bgObjectItem-Inner ">
+        <div class="js-image" style="animation-duration: 1.13207s; animation-delay: -3.16601s;" alt="" decoding="async">
         </div>
       </div>
-      <div class="bgObjectItem">
-        <div class="bgObjectItem-Inner">
-          <img class="js-image" src="https://static.igem.wiki/teams/4584/wiki/bg-objects/bgobject.png" style="animation-duration: 3.13207s; animation-delay: -3.16601s;" alt="" decoding="async">
-        </div>
+    </div>
+    <div class="bgObjectItem">
+      <div class="bgObjectItem-Inner">
+        <img class="js-image" src="https://static.igem.wiki/teams/4584/wiki/bg-objects/bgobject.png"
+          style="animation-duration: 3.13207s; animation-delay: -3.16601s;" alt="" decoding="async">
       </div>
-      <div class="bgObjectItem">
-        <div class="bgObjectItem-Inner">
-          <img class="js-image" src="https://static.igem.wiki/teams/4584/wiki/bg-objects/bgobject.png" style="animation-duration: 3.13207s; animation-delay: -3.16601s;" alt="" decoding="async">
-        </div>
+    </div>
+    <div class="bgObjectItem">
+      <div class="bgObjectItem-Inner">
+        <img class="js-image" src="https://static.igem.wiki/teams/4584/wiki/bg-objects/bgobject.png"
+          style="animation-duration: 3.13207s; animation-delay: -3.16601s;" alt="" decoding="async">
       </div>
-      <div class="bgObjectItem">
-        <div class="bgObjectItem-Inner">
-          <img class="js-image" src="https://static.igem.wiki/teams/4584/wiki/bg-objects/bgobject.png" style="animation-duration: 3.13207s; animation-delay: -3.16601s;" alt="" decoding="async">
-        </div>
+    </div>
+    <div class="bgObjectItem">
+      <div class="bgObjectItem-Inner">
+        <img class="js-image" src="https://static.igem.wiki/teams/4584/wiki/bg-objects/bgobject.png"
+          style="animation-duration: 3.13207s; animation-delay: -3.16601s;" alt="" decoding="async">
       </div>
-
     </div>
 
-    <section class="index__page__inner">
-      <section class="headBlock__main">
-        <div class="container">
-          <div class="headBlock__inner">
-            <div class="headBlock__titlesWrapper">
-              <div class="index_headBlockImage-container" data-aos="fade-right" data-aos-duration="1250"
-                data-aos-delay="1300" data-aos-once="true">
-                <img src="https://static.igem.wiki/teams/4584/wiki/gv.png" alt="" decoding="async">
-              </div>
-              <h1 class="headBlock__title__container">
-                <span class="headBlock__subtitle__line " data-aos="fade-left" data-aos-duration="0"
-                  data-aos-delay="0" data-aos-once="true">
-                  Introducing
-                </span>
-                <span class="headBlock__title__line  " data-aos="fade-right" data-aos-duration="500"
-                  data-aos-delay="500" data-aos-once="true">ColoEcho</span>
-                <span class="headBlock__description__line" data-aos="fade-up" data-aos-duration="1250"
-                  data-aos-delay="750" data-aos-once="true">
-                  A diagnostic tool for Colorectal Cancer
-                </span>
-              </h1>
+  </div>
+
+  <section class="index__page__inner">
+    <section class="headBlock__main">
+      <div class="container">
+        <div class="headBlock__inner">
+          <div class="headBlock__titlesWrapper">
+            <div class="index_headBlockImage-container" data-aos="fade-right" data-aos-duration="1250"
+              data-aos-delay="1300" data-aos-once="true">
+              <img src="https://static.igem.wiki/teams/4584/wiki/gv.png" alt="" decoding="async">
             </div>
+            <h1 class="headBlock__title__container">
+              <span class="headBlock__subtitle__line " data-aos="fade-left" data-aos-duration="0" data-aos-delay="0"
+                data-aos-once="true">
+                Introducing
+              </span>
+              <span class="headBlock__title__line  " data-aos="fade-right" data-aos-duration="500" data-aos-delay="500"
+                data-aos-once="true">ColoEcho</span>
+              <span class="headBlock__description__line" data-aos="fade-up" data-aos-duration="1250" data-aos-delay="750"
+                data-aos-once="true">
+                A diagnostic tool for Colorectal Cancer
+              </span>
+            </h1>
           </div>
         </div>
+      </div>
 
-      </section>
-      <section class="index-section">
-        <div class="container">
-          <div class="row">
-            <div class="col" data-aos="fade-up" data-aos-duration="900">
-              <img data-src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822" class="person-img"
-                alt="DOG" src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822">
-            </div>
+    </section>
+    <section class="index-section">
+      <div class="container">
+        <div class="row">
+          <div class="col" data-aos="fade-up" data-aos-duration="900">
+            <img src="https://static.igem.wiki/teams/4584/wiki/planet.png" class="person-img"></div>
             <div class="col" data-aos="fade-up" data-aos-duration="1200">
               <h1 class="display-4" id="index-title">Project: ColoEcho</h1>
               <p class="index-text">
@@ -65,117 +67,110 @@
                 aiming
                 to increase patient chances of survival.
               </p>
-              <a type="button" class="btn btn-outline-primary" href="./description">Read More</a>
+              <a type="button" class="btn btn-outline-light" href="./description">Read More</a>
             </div>
           </div>
         </div>
-      </section>
-      <section class="index-section">
-        <div class="container">
-          <div class="row">
-            <div class="col-6" data-aos="fade-up" data-aos-duration="1200">
-              <h1 class="display-4" id="index-title">The Problem</h1>
-              <p class="index-text">
-                Colorectal cancer ranks among the significant contributors to cancer-related mortality, responsible for a
-                substantial portion of cancer-related deaths.
-                Approximately 15% of all cancer fatalities are attributed to colorectal cancer.
-                This cancer type claims more lives annually than certain other prevalent cancers like ovarian and liver
-                cancers,
-                underscoring its impact. Regrettably, symptoms typically manifest in the later stages of the disease when
-                treatment options are limited,
-                resulting in diminished chances of survival.
-                Presently employed diagnostic methods lack ease, precision, and patient well-being.
-                As a result, there is an imperative need to develop a diagnostic strategy that offers improved patient
-                accessibility and
-                addresses the limitations observed in current methodologies.</p>
-              <a type="button" class="btn btn-outline-primary" href="./description">Read More</a>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="index-section">
-
-
-
-        <div class="container">
-          <div class="row justify-content-center">
-
-            <div data-aos="fade-up" data-aos-duration="1000" class="col-9 aos-init aos-animate ">
-              <h3 class="display-1" id="index-title" style="text-align: center !important;">Our technique</h3>
-              <p class="index-text" style="text-align: center !important;">
-                Combining the technology of Ultrasound Molecular Imaging and Gas Vesicles, we have designed a system that
-                can detect specific biomarkers,
-                that colorectal cancer cells overexpress.
-              </p>
-            </div>
-          </div>
-          <div class="row justify-content-center" id="index-cards">
-            <div data-aos="fade-up-right" data-aos-duration="1000" class="col-lg-4 aos-init aos-animate">
-              <div class="card text-center border-light " style="background: transparent;">
-                <div class="card-body">
-                  <h4 class="card-title">Gas Vesicles</h4>
-                  <h5 class="card-subtitle">It provides a patient-friendly diagnostic approach.</h5>
-                  <p class="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada mi non eros vestibulum, ac
-                    tristique justo vestibulum. Sed nec faucibus nunc. Nulla facilisi. Suspendisse potenti. Cras consequat
-                    neque eu risus posuere, ut blandit est hendrerit.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div data-aos="fade-up-left" data-aos-duration="1000" class="col-lg-4 aos-init aos-animate">
-              <div class="card text-center border-light " style="background: transparent;">
-                <div class="card-body">
-                  <h4 class="card-title">Ultrasound</h4>
-                  <h5 class="card-subtitle">It provides a patient-friendly diagnostic approach.</h5>
-                  <p class="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada mi non eros vestibulum, ac
-                    tristique justo vestibulum. Sed nec faucibus nunc. Nulla facilisi. Suspendisse potenti. Cras consequat
-                    neque eu risus posuere, ut blandit est hendrerit.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-      </section>
-      <section class="container">
+    </section>
+    <section class="index-section">
+      <div class="container">
         <div class="row">
-          <div class="col tabbed gallery">
-            <ul class="thumbnails">
-              <li>
-                <div class="c-borderedImage index_p-headBlockImage js-indexHeadBlockImage --portrait"
-                  data-is-loaded="true"
-                  style="transform: translate(-50%, -50%) rotate(-1.83337deg); transition-delay: 0s;"
-                  data-is-playing="true">
-                  <div class="c-borderedImage__container js-borderedImageContainer"
-                    style="transition-duration: 1.2s; transform: rotate(-4.76875deg); transition-delay: 0s;">
-                    <div class="c-borderedImage__inner js-borderedImageInner"
-                      style="transition-duration: 1.2s; transform: translateX(1.7268%); transition-delay: 0s;">
-                      <picture>
-                        <source srcset="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822"
-                          type="image/png">
-                        <img class="js-borderedImageImage"
-                          src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822" alt=""
-                          decoding="async">
-                      </picture>
-                    </div>
+          <div class="col-6" data-aos="fade-up" data-aos-duration="1200">
+            <h1 class="display-4" id="index-title">The Problem</h1>
+            <p class="index-text">
+              According to Cancer Research UK, about 10% of those diagnosed with stage 4 colorectal cancer will survive
+              for 5 years or more post-diagnosis, but if diagnosed in the earlier stages (stages 1 and 2), about 85-90% of
+              people will survive for 5 years or more.
+
+              This problem does not appear to be resolving as the WHO expects an increase of 3.2 million new cases per
+              year by 2040 and 1.6 million deaths per year, a 73% increase. In the UK alone, colorectal cancer is the
+              second most deadly cancer, constituting for 10% of all cancer deaths.</p>
+            <a type="button" class="btn btn-outline-primary" href="./description">Read More</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="index-section">
+
+
+
+      <div class="container">
+        <div class="row justify-content-center">
+
+          <div data-aos="fade-up" data-aos-duration="1000" class="col-9 aos-init aos-animate ">
+            <h3 class="display-1" id="index-title" style="text-align: center !important;">Our technique</h3>
+            <p class="index-text" style="text-align: center !important;">
+              Combining the technology of Ultrasound Molecular Imaging and Gas Vesicles, we have designed a system that
+              can detect specific biomarkers,
+              that colorectal cancer cells overexpress.
+            </p>
+          </div>
+        </div>
+        <div class="row justify-content-center" id="index-cards">
+          <div data-aos="fade-up-right" data-aos-duration="1000" class="col-lg-4 aos-init aos-animate">
+            <div class="card text-center border-light " style="background: transparent;">
+              <div class="card-body">
+                <h4 class="card-title">Gas Vesicles</h4>
+                <h5 class="card-subtitle">It provides a patient-friendly diagnostic approach.</h5>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada mi non eros vestibulum, ac
+                  tristique justo vestibulum. Sed nec faucibus nunc. Nulla facilisi. Suspendisse potenti. Cras consequat
+                  neque eu risus posuere, ut blandit est hendrerit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div data-aos="fade-up-left" data-aos-duration="1000" class="col-lg-4 aos-init aos-animate">
+            <div class="card text-center border-light " style="background: transparent;">
+              <div class="card-body">
+                <h4 class="card-title">Ultrasound</h4>
+                <h5 class="card-subtitle">It provides a patient-friendly diagnostic approach.</h5>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada mi non eros vestibulum, ac
+                  tristique justo vestibulum. Sed nec faucibus nunc. Nulla facilisi. Suspendisse potenti. Cras consequat
+                  neque eu risus posuere, ut blandit est hendrerit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+    <section class="container">
+      <div class="row">
+        <div class="col tabbed gallery">
+          <ul class="thumbnails">
+            <li>
+              <div class="c-borderedImage index_p-headBlockImage js-indexHeadBlockImage --portrait" data-is-loaded="true"
+                style="transform: translate(-50%, -50%) rotate(-1.83337deg); transition-delay: 0s;"
+                data-is-playing="true">
+                <div class="c-borderedImage__container js-borderedImageContainer"
+                  style="transition-duration: 1.2s; transform: rotate(-4.76875deg); transition-delay: 0s;">
+                  <div class="c-borderedImage__inner js-borderedImageInner"
+                    style="transition-duration: 1.2s; transform: translateX(1.7268%); transition-delay: 0s;">
+                    <picture>
+                      <source srcset="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822"
+                        type="image/png">
+                      <img class="js-borderedImageImage"
+                        src="https://www.dogecore.com/cdn/shop/files/thicc_100x.png?v=1669641822" alt="" decoding="async">
+                    </picture>
                   </div>
                 </div>
-              </li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
+              </div>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
         </div>
-      </section>
+      </div>
     </section>
+  </section>
 </template>
 <script>
 
